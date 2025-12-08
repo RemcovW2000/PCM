d2c437163da6c2e5394bb710fe8ddcfd981afb89
remcovanwoerkom <RemcovW2000@gmail.com>
2025-12-08 18:31:44 +0100

    1st question done.

--- cure kinetics/q1.py
+++ cure kinetics/q1.py
@@ -5,7 +5,8 @@ from typing import Dict, List
 import re
 
 from matplotlib import pyplot as plt
-from resources.constants import START_TIME_120, START_TIME_150
+from resources.constants import START_TIME_120, START_TIME_150, SAMPLE_WEIGHT_120, \
+    SAMPLE_WEIGHT_150, SAMPLE_WEIGHT_180
 from scipy.signal import butter, filtfilt
 
 
@@ -51,7 +52,7 @@ def apply_lowpass_filter(data: list[float], time: list[float], cutoff_freq: floa
     x_lp = filtfilt(b, a, x)
     return x_lp.tolist()
 
-def prepare_for_plotting(data: Dict[str, list[float]], start_time: float = 0.0) ->Dict[str, list[float]]:
+def prepare_for_plotting(data: Dict[str, list[float]], sample_weight: float, start_time: float = 0.0) ->Dict[str, list[float]]:
     """
     Prepare the data for plotting by filtering and normalizing.
     """
@@ -67,6 +68,9 @@ def prepare_for_plotting(data: Dict[str, list[float]], start_time: float = 0.0) 
     # subtract baseline (which is 0 in the dataset)
     net_heat_flow = unsubtracted_heat_flow - baseline_heat_flow
 
+    # Convert to W/g
+    net_heat_flow = net_heat_flow / sample_weight
+
     # Normalize to 0 mW at the end of the measurement
     final_heat_flow_at_end = net_heat_flow[-100:].mean()
     net_heat_flow -= final_heat_flow_at_end
@@ -75,17 +79,21 @@ def prepare_for_plotting(data: Dict[str, list[float]], start_time: float = 0.0) 
     index_first_exotherm = next(
         i for i, v in enumerate(net_heat_flow) if v > 0)
 
+    # Find the index corresponding to the specified start time
     index_at_start_time = next(
         i for i, t in enumerate(time) if t >= start_time)
 
     first_index = max(index_first_exotherm, index_at_start_time)
 
+    # copy data from the given data dict.
     data_out = {}
     for key, item in data.items():
         data_out[key] = item[first_index:]
 
+    # add net heat flow
     data_out['Net Heat Flow'] = net_heat_flow[first_index:].tolist()
 
+    # apply low-pass filter to heat flow
     data_out['Filtered Heat Flow'] = apply_lowpass_filter(
         data_out['Net Heat Flow'], data_out['Time'], cutoff_freq=1)
     return data_out
@@ -94,7 +102,7 @@ def prepare_for_plotting(data: Dict[str, list[float]], start_time: float = 0.0) 
 isothermal_120_path = Path(__file__).parent / "resources/isothermal_120.txt"
 data_120 = read_sheet_to_dict(isothermal_120_path)
 
-final_data_120 = prepare_for_plotting(data_120, start_time=START_TIME_120)
+final_data_120 = prepare_for_plotting(data_120, sample_weight= SAMPLE_WEIGHT_120,start_time=START_TIME_120)
 time_120 = final_data_120['Time']
 net_heat_flow_120 = final_data_120['Filtered Heat Flow']
 
@@ -102,13 +110,13 @@ net_heat_flow_120 = final_data_120['Filtered Heat Flow']
 isothermal_150_path = Path(__file__).parent / "resources/isothermal_150.txt"
 data_150 = read_sheet_to_dict(isothermal_150_path)
 
-final_data_150 = prepare_for_plotting(data_150, start_time=START_TIME_150)
+final_data_150 = prepare_for_plotting(data_150, sample_weight= SAMPLE_WEIGHT_150,start_time=START_TIME_150)
 time_150 = final_data_150['Time']
 net_heat_flow_150 = final_data_150['Filtered Heat Flow']
 
 isothermal_180_path = Path(__file__).parent / "resources/isothermal_180.txt"
 data_180 = read_sheet_to_dict(isothermal_180_path)
-final_data_180 = prepare_for_plotting(data_180)
+final_data_180 = prepare_for_plotting(data_180, sample_weight=SAMPLE_WEIGHT_180)
 time_180 = final_data_180['Time']
 net_heat_flow_180 = final_data_180['Filtered Heat Flow']
 
@@ -119,7 +127,7 @@ plt.xlim(0, 200)
 plt.axhline(y=0, color='black', linestyle='--')
 plt.axvline(x=0, color='black', linestyle='--')
 plt.xlabel('Time (s)')
-plt.ylabel('Net Heat Flow (mW)')
-plt.title('Net Heat Flow vs. Time')
+plt.ylabel('Net specific heat flow (W/g)')
+plt.title('Net specific heat flow vs. Time')
 plt.legend()
 plt.show()
