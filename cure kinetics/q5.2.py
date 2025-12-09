bd621a33ca24c15671dc91fa9a259aea79812938
remcovanwoerkom <RemcovW2000@gmail.com>
2025-12-09 20:33:34 +0100

    use E1 correctly

--- cure kinetics/q5.2.py
+++ cure kinetics/q5.2.py
@@ -1,11 +1,12 @@
 import numpy as np
 from q1 import final_data_120, final_data_150, final_data_180
-from q5 import A1 ,fraction_cured_120, fraction_cured_150, fraction_cured_180, dalpha_dt_120, dalpha_dt_150, dalpha_dt_180
+from q5 import A1, fraction_cured_120, fraction_cured_150, fraction_cured_180, \
+    dalpha_dt_120, dalpha_dt_150, dalpha_dt_180, E1
 
 
 def da_dt(A2, E2, m, n, alpha, T):
     R = 8.314  # J/(mol·K)
-    term_1 = A1 * np.exp(-E2 / (R * T))
+    term_1 = A1 * np.exp(-E1 / (R * T))
     term_2 = A2 * np.exp(-E2 / (R * T)) * alpha ** m
     return (term_1 + term_2) * (1 - alpha)**n
 
@@ -39,7 +40,7 @@ def find_dalpha_dt_at_alphas(fraction_cured_lst:list[float], dalpha_dt_lst: list
         dalpha_dt_vals.append(np.interp(alpha, fraction_cured_lst, dalpha_dt_lst))
     return dalpha_dt_vals
 
-alpha_vals = np.linspace(0, 0.97, 1000)
+alpha_vals = np.linspace(0.01, 0.97, 1000)
 dalpha_dt_vals = {}
 dalpha_dt_vals[120] = find_dalpha_dt_at_alphas(
     fraction_cured_lst=fraction_cured_120,
@@ -63,10 +64,10 @@ if __name__ == "__main__":
     from scipy.optimize import minimize
 
     # initial guesses
-    A2_init = 1e5
-    E2_init = 9e4  # J/mol
-    m_init = 2.0
-    n_init = 2.0
+    A2_init = 1e6
+    E2_init = 6e4  # J/mol
+    m_init = 1
+    n_init = 1.25
 
     result = minimize(
         lambda params: calculate_error(
@@ -78,7 +79,7 @@ if __name__ == "__main__":
             alpha_vals=alpha_vals
         ),
         x0=[A2_init, E2_init, m_init, n_init],
-        bounds=[(1e3, 1e10), (1000.0, 200000.0), (0.0, 5.0), (0.0, 5.0)]
+        bounds=[(1e3, 1e10), (1000.0, 200000.0), (0.0, 2.4), (0.0, 2.5)]
     )
 
     A2_opt, E2_opt, m_opt, n_opt = result.x
