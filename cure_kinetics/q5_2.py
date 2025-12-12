72e6e00bdefb5627e9275b4520a5e5406bdce014
remcovanwoerkom <RemcovW2000@gmail.com>
2025-12-12 20:57:49 +0100

    plot cure rate vs doc.

--- cure_kinetics/q5_2.py
+++ cure_kinetics/q5_2.py
@@ -1,20 +1,10 @@
+import numpy as np
 from scipy.optimize import least_squares
 
-from cure_kinetics.q3 import cure_rate_120, cure_rate_180
-from q5 import (
-    A1_solution, E1_solution,
-    fraction_cured_120, dalpha_dt_120,
-    fraction_cured_150, dalpha_dt_150,
-    fraction_cured_180, dalpha_dt_180
-)
+from resources.constants import R
 
-import numpy as np
-from resources.constants import START_TIME_150, START_TIME_120
-from q1 import final_data_150, final_data_120, final_data_180
-from q3 import fraction_cured_150, fraction_cured_120, fraction_cured_180
-from q5 import E1_solution, A1_solution
-
-R = 8.314
+from q3 import fraction_cured_120, fraction_cured_180, cure_rate_120, cure_rate_180
+from q5 import E1_solution, A1_solution, dalpha_dt_120, dalpha_dt_180
 
 def k1(T):
     return A1_solution * np.exp(-E1_solution / (R * T))
@@ -89,7 +79,7 @@ def da_dt(A1, E1, A2, E2, m, n, alpha, T):
 def simulate_cure(A1, E1, A2, E2, m, n, T_C, t_lst):
     T = T_C + 273.15
     alpha = 0.0
-    alpha_vals = [0.0]
+    alpha_vals = []
     da_dt_vals = []
     for i in range(1, len(t_lst)):
         dt = t_lst[i] - t_lst[i-1]
@@ -98,23 +88,23 @@ def simulate_cure(A1, E1, A2, E2, m, n, T_C, t_lst):
 
         da_dt_vals.append(dadt)
         alpha_vals.append(alpha)
-    return np.array(da_dt_vals)
+    return np.array(alpha_vals), np.array(da_dt_vals)
 
 if __name__ == "__main__":
     from matplotlib import pyplot as plt
-    # Example parameters
-    t_lst = np.linspace(0, 30000, 1000000)
+    t_lst = np.linspace(0, 30000, 10000)
 
     for cure_temp in [120, 180]:
         sim_results = simulate_cure(A1_solution, E1_solution, A2_solution, E2_solution, m_solution, n_solution, cure_temp, t_lst)
-        plt.plot(t_lst[:-1], sim_results, label=f'Simulated {cure_temp}°C')
+        alpha_vals = sim_results[0]
+        da_dt_vals = sim_results[1]
+        plt.plot(alpha_vals, da_dt_vals, label=f'Simulated {cure_temp}°C')
 
-    plt.plot([t - START_TIME_120*60 for t in final_data_120["Time Seconds"]], cure_rate_120, '--', label='Experimental 120°C')
-    plt.plot([t -2.5*60 for t in final_data_180["Time Seconds"]], cure_rate_180, '--', label='Experimental 180°C')
+    plt.plot(fraction_cured_120, cure_rate_120, '--', label='Experimental 120°C')
+    plt.plot(fraction_cured_180, cure_rate_180, '--', label='Experimental 180°C')
     plt.xlabel('Time (s)')
     plt.ylabel('Degree of Cure (α)')
-    plt.title('Cure rate vs. Time, simulated vs. experimental')
-    plt.xlim(0, 5000)
+    plt.title('Cure rate vs. degree of cure, simulated vs. experimental')
     plt.axhline(0, color='k', linestyle='--')
     plt.legend()
     plt.show()
